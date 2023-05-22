cb95797d754d46040045b812fbf53f32d8662527
Ahmet2732 <Ahmedabdelkkader10@gmail.com>
2023-05-22 03:57:11 +0300

    item collection

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInventory : MonoBehaviour
+{
+public int NumberOfDiamonds{get; private set;}
+public UnityEvent<PlayerInventory> onDiamondCollected;
+
+public  void DiamonCollected(){
+    
+NumberOfDiamonds++;
+onDiamondCollected.Invoke(this);
+}
+}

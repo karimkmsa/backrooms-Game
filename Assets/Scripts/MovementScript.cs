d31cc3838fbf7ce861cc1b590a33ca33468c266e
MichaelMagdyKhairElSaid <michaelkhairelsaid@gmail.com>
2023-05-22 22:33:45 +0300

    Michael

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
-{
+{   
+ private Animator animator; // added for animation
     public bool isMoving = false;
     public float movementSpeed = 20;
     public float  Speed = 125;
@@ -19,37 +20,48 @@ public class MovementScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>(); // add for animation
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) ||Input.GetKey(KeyCode.S) ||Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.A))
         {
+
+            if(Input.GetKey(KeyCode.W))
+        { animator.SetBool("is moving",true);
             transform.position += transform.forward * Time.deltaTime * movementSpeed;
             isMoving = true;
             if(Input.GetKey(KeyCode.LeftShift) & isMoving)
          {
             transform.position += transform.forward * Time.deltaTime * Speed;
+             animator.SetBool("isRuning",true);
+          }else{
+               animator.SetBool("isRuning",false);
           }
         }
 
         if(Input.GetKey(KeyCode.S))
-        {
+        { animator.SetBool("is moving",true);
             transform.position -= transform.forward * Time.deltaTime * movementSpeed;
         }
 
         if(Input.GetKey(KeyCode.D))
-        {
+        {animator.SetBool("is moving",true);
             transform.position += transform.right * Time.deltaTime * movementSpeed;
         }
 
         if(Input.GetKey(KeyCode.A))
-        {
+        {animator.SetBool("is moving",true);
             transform.position -= transform.right * Time.deltaTime * movementSpeed;
         }
-       
+        }
+       else{
+           animator.SetBool("is moving",false);
+            animator.SetBool("isRuning",false);
+       }
 
         ShootRaycast();
 
---
+++ Assets/Scripts/animation.cs
@@ -0,0 +1,34 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class animation : MonoBehaviour
+// {
+//      private Animator animator; // added for animation
+//       // add for animation
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         animator = GetComponent<Animator>();
+    
+//     }
+
+//     // Update is called once per frame
+//     void Update(){
+// bool isrunning = animator.GetBool("isRunning");
+// bool isWalking = animator. GetBool(isWalkingHash) ;
+// bool forwardPressed = Input.GetKey("w");
+// bool runPressed = Input.GetKey("left shift");
+
+// //if player is pressing W or A or S or D
+// if (!isWalking&&forwardPressed){
+//     animator.SetBool(isWalkingHash,true);
+//     }
+
+// //if player is not pressing W or A or S or D
+// if (isWalking&&!forwardPressed){
+//     animator.SetBool(isWalkingHash,true);
+// }
+    
+//     }
+// }
